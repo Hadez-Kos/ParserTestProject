1767fb10a0a9ab09a869ce72bf716e3f9690db09
Andrey <andreykosov21@gmail.com>
2021-11-12 23:29:34 +0300

    Этап 2 выгрузки данных
    Сделана геолокация сайта по городам, также сделана выгрузка данных товаров, даже которых нет в наличии.

--- Parser.py
+++ Parser.py
@@ -1,27 +1,52 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import time
+import selenium.webdriver.common.devtools.v93 as devtools
 
 options = webdriver.ChromeOptions()
-category_product = {}
+
+options.add_experimental_option('prefs', {
+    'geolocation': True
+})
+
 try:
     driver = webdriver.Chrome(
         executable_path='yandexdriver.exe',
         options=options
     )
 
-    driver.get(url='https://www.detmir.ru')
-    time.sleep(5)
+    url = 'https://www.detmir.ru/catalog/index/name/zdorovyj_perekus_pp/'
 
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    for category in soup.find('div', class_='kX').find('ul', class_='kL').find_all('li', attrs={'class': 'kM'}):
-        tag_a = category.find('a', class_='kN')
-        if not (tag_a is None) and 'catalog' in tag_a.get('href'):
-            category_product[tag_a.text] = tag_a.get('href')
-    print(category_product)
+    driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+        'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 YaBrowser/21.11.0.1996 Yowser/2.5 Safari/537.36',
+        'platform': 'Windows'})
+
+    driver.execute_cdp_cmd("Emulation.setGeolocationOverride", {"latitude": 59.9386,
+                                                                "longitude": 30.3141,
+                                                                "accuracy": 50})
+
+    page = 1
+    while True:
+        url_page = url + f"page/{page}/"
+        driver.get(url=url_page)
+
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        try:
+            html_tag_product = soup.find('div', class_='cc').find('div', class_='cj').find('div', class_='oX')
+            city = soup.find('span', class_='yV').text
+            for i in html_tag_product.find_all('div', attrs={
+                'class': ['wR w_2 oY wW', 'wR w_2 oY wW w_0', 'wR wZ oY wU', 'wR w_2 oY']}):
+                url_product = i.find('a', attrs={'class': ['Ni NK', 'Ni NK Nj']}).get('href')
+                Id_product = int(url_product.split('/id', 1)[1].replace('/', ''))
+                title_product = i.find('p', class_='Nl').text
+                print(url_product, Id_product, title_product, city, page)
+
+            page += 1
+        except:
+            break
+
+
 except Exception as ex:
     print(ex)
 finally:
     driver.close()
     driver.quit()
-
